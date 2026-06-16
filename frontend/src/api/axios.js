70b1cfaad0a72e1a134fd548cfdaf4ac94d19d4e
sai <pvsirisha44@gmail.com>
2026-06-16 16:25:38 +0530

    ..

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,15 @@
 import axios from "axios";
-const API=axios.create({
-    baseURL:"http://localhost:8000",
+
+const API = axios.create({
+  baseURL: "https://internship-aggregator-production.up.railway.app",
 });
 
-API.interceptors.request.use((config)=>{
-    const token=localStorage.getItem("token");
-    if(token){
-        config.headers.Authorization=`Bearer ${token}`;
-    }
-    return config;
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
+
 export default API;
