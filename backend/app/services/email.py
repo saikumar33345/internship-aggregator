b52483705031a866a1726e4fc5d8c0f23cff813f
sai <pvsirisha44@gmail.com>
2026-06-16 17:39:51 +0530

    gmail fix

--- backend/app/services/email.py
+++ backend/app/services/email.py
@@ -8,10 +8,10 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("EMAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("EMAIL_PASSWORD"),
     MAIL_FROM=os.getenv("EMAIL_USERNAME"),
-    MAIL_PORT=587,
+    MAIL_PORT=465,
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
 )
 
