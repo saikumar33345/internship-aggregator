4f2b1d5694e1392a491bb0919cd85d5cd59e700b
sai <pvsirisha44@gmail.com>
2026-06-16 16:30:33 +0530

    cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,8 +19,8 @@ app=FastAPI(title="Internship Aggregator API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://internship-aggregator-brown.vercel.app/"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
